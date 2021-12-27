8e8f50745d31662327a8d97f10d9e9d6e535dd84
Thein Khant <thurakhantthein4425@gmail.com>
2021-12-27 19:30:47 +0630

    exit_btn

---
+++ exit_btn/lib/exit_btn.dart
@@ -0,0 +1,50 @@
+library exit_btn;
+
+import 'package:flutter/material.dart';
+
+class ThemeExit extends StatelessWidget {
+  final String text;
+
+  const ThemeExit({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const textColor = Color.fromRGBO(46, 76, 109, 1);
+    const buttonColor = Color.fromRGBO(230, 242, 246, 1);
+    const iconColor = Color.fromRGBO(46, 76, 109, 1);
+    const style1 = TextStyle(
+      color: textColor,
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+    );
+    return Container(
+      width: 220,
+      height: 60,
+      decoration: BoxDecoration(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: TextButton(
+        onPressed: () {},
+        child: Row(
+          children: [
+            const Expanded(flex: 1, child: SizedBox()),
+            Expanded(
+                flex: 3,
+                child: Text(text, textAlign: TextAlign.center, style: style1)),
+            const Expanded(
+              flex: 1,
+              child: Icon(
+                Icons.exit_to_app_rounded,
+                color: iconColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
